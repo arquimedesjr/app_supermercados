251cf0d6212ac6485bcb379dcd48bd7535c3eaa9
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-02-12 15:34:47 -0300

    project supermercado

---
+++ models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Create your models here.
+class Marca(models.Model):
+    nome = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    descricao = models.CharField(max_length=50, null=False)
+    preco = models.FloatField(null=False)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    categoria = models.ManyToManyField(Categoria)
+
+    def __str__(self):
+        return self.nome
